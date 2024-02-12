976f6a1508af4c08b8fb15fd9ce72ae60a071e5e
Kinshuk Banerjee <kinshuk2002.king@gmail.com>
2024-02-12 17:02:47 +0530

    Update Largest-Divisible-Subset.java

--- Largest-Divisible-Subset.java
+++ Largest-Divisible-Subset.java
@@ -1,1 +1,28 @@
+class Solution {
+    Map<Integer,List<Integer>> map = new HashMap<>();
+    List<Integer> checkDiv(int[] nums, int i){
+        if(map.containsKey(i)){
+            return map.get(i);
+        }
 
+        List<Integer> Muls = new ArrayList<>();
+        
+        int div = i == 0 ? 1 : nums[i - 1];
+        for(int k = i; k < nums.length; k++){
+            if(nums[k] % div == 0){
+                List<Integer> arr = new ArrayList<>(checkDiv(nums, k + 1));
+                arr.add(nums[k]);
+                if(arr.size() > Muls.size()){
+                    Muls = arr;
+                }
+            }
+        }
+
+        map.put(i, Muls);
+        return Muls;
+    }
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        Arrays.sort(nums);
+        return checkDiv(nums, 0);    
+    }
+}
